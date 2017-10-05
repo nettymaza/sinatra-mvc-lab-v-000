d30329337d4a4eec3c001a3830c20432ed468775
Annette Maza <nettymaza@gmail.com>
2017-10-05 15:45:46 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,6 +5,7 @@ class Piglatinizer
   def piglatinize(word)
     alphabet = ('a'..'z').to_a
     vowels = %w[a e i o u A E I O U]
+    consonants = alphabet - vowels
 
     if vowels.include?(word[0])
       word + "way"
