317553ff0e188bef87278a77a6bc2e8faa285213
Annette Maza <nettymaza@gmail.com>
2017-10-05 15:44:26 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,10 +3,13 @@ require 'pry'
 class Piglatinizer
 
   def piglatinize(word)
+    alphabet = ()
     vowels = %w[a e i o u A E I O U]
 
     if vowels.include?(word[0])
       word + "way"
+    elsif 
+      
   
 
   end
