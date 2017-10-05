4a89c14830a4121ff22d3113e1bc6e1690e0f2f7
Annette Maza <nettymaza@gmail.com>
2017-10-05 15:45:57 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,6 +6,7 @@ class Piglatinizer
     alphabet = ('a'..'z').to_a
     vowels = %w[a e i o u A E I O U]
     consonants = alphabet - vowels
+    binding.pry
 
     if vowels.include?(word[0])
       word + "way"
