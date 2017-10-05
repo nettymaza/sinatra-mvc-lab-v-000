4518ec1c4fd8656cb27133b4d195a433a3996e41
Annette Maza <nettymaza@gmail.com>
2017-10-05 16:10:38 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,6 +13,7 @@ class Piglatinizer
     elsif consonants.include?(word[0]) && consonants.include?(word[1])
       word[2..-1] + word[0..1] + 'ay'
     elsif consonants.include?(word[0])
+       word[1..-1] + word[0] + 'ay'
 
     end
 
