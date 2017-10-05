d8daafeb3369b4b8953576e3470022db748cc040
Annette Maza <nettymaza@gmail.com>
2017-10-05 15:47:02 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,13 @@
 require 'pry'
 
+
 class Piglatinizer
 
   def piglatinize(word)
     alphabet = ('a'..'z').to_a
     vowels = %w[a e i o u A E I O U]
     consonants = alphabet - vowels
-    binding.pry
+  
 
 
   end
