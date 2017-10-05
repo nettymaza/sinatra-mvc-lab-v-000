dd5eaa322f2a1bddf789a9da15aae9fe463a24ca
Annette Maza <nettymaza@gmail.com>
2017-10-05 16:09:34 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,7 +12,7 @@ class Piglatinizer
       word << "way"
     elsif consonants.include?(word[0]) && consonants.include?(word[1])
       word[2..-1] + word[0..1] + 'ay'
-      
+
     end
 
     binding.pry
