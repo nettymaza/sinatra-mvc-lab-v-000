7620d2b2e70d9449a7002c897f72d62a8d4cd587
Annette Maza <nettymaza@gmail.com>
2017-10-05 16:08:47 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,7 +11,8 @@ class Piglatinizer
     if vowels.include?(word[0])
       word << "way"
     elsif consonants.include?(word[0]) && consonants.include?(word[1])
-      word
+      word[2..-1] + word[0..1] + 'ay'
+      
     end
 
     binding.pry
